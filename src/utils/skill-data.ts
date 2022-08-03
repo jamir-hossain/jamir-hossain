ff1202a917c23ba92a85bce9792f35d69e4297f7
Jamir <contact.jamir@gmail.com>
2022-08-04 01:12:46 +0600

    adding skills

--- src/utils/skill-data.ts
+++ src/utils/skill-data.ts
@@ -11,6 +11,63 @@ const skillData = [
       { image: "/assets/skills/typescript.png", name: "TypeScript" },
     ],
   },
+  {
+    title_first: "Expertise",
+    title_second: "With",
+    items: [
+      { image: "/assets/skills/material-ui.png", name: "Material-UI" },
+      { image: "/assets/skills/tailwind-css.png", name: "Tailwind-CSS" },
+      { image: "/assets/skills/react.png", name: "React JS" },
+      { image: "/assets/skills/nextjs.png", name: "Next JS" },
+      { image: "/assets/skills/laravel.png", name: "Laravel" },
+      { image: "/assets/skills/mysql.png", name: "MySQL" },
+    ],
+  },
+  {
+    title_first: "Comfortable",
+    title_second: "With",
+    items: [
+      { image: "/assets/skills/sass.png", name: "SASS" },
+      { image: "/assets/skills/redux.png", name: "Redux" },
+      { image: "/assets/skills/react-query.png", name: "React-Query" },
+      { image: "/assets/skills/node-js.png", name: "Node JS" },
+      { image: "/assets/skills/express.png", name: "Express" },
+      { image: "/assets/skills/mongodb.png", name: "MongoDB" },
+    ],
+  },
+  {
+    title_first: "Familiar",
+    title_second: "With",
+    items: [
+      { image: "/assets/skills/firebase.png", name: "Firebase" },
+      { image: "/assets/skills/inertiajs.png", name: "Inertia JS" },
+    ],
+  },
+  // {
+  //   title_first: "Web",
+  //   title_second: "Frontend",
+  //   items: [
+  //     { image: "/assets/skills/html5.png", name: "HTML5" },
+  //     { image: "/assets/skills/css3.png", name: "CSS3" },
+  //     { image: "/assets/skills/bootstrap.png", name: "Bootstrap" },
+  //     { image: "/assets/skills/sass.png", name: "SASS" },
+  //     { image: "/assets/skills/material-ui.png", name: "Material-UI" },
+  //     { image: "/assets/skills/tailwind-css.png", name: "Tailwind-CSS" },
+  //     { image: "/assets/skills/react.png", name: "React JS" },
+  //     { image: "/assets/skills/nextjs.png", name: "Next JS" },
+  //   ],
+  // },
+  // {
+  //   title_first: "Web",
+  //   title_second: "Backend",
+  //   items: [
+  //     { image: "/assets/skills/laravel.png", name: "Laravel" },
+  //     { image: "/assets/skills/node-js.png", name: "Node JS" },
+  //     { image: "/assets/skills/express.png", name: "Express" },
+  //     { image: "/assets/skills/mysql.png", name: "MySQL" },
+  //     { image: "/assets/skills/mongodb.png", name: "MongoDB" },
+  //   ],
+  // },
 ];
 
 export default skillData;
